66a84c0269aa79ff929e7282aa5e3163a0be04dc
Seth <setholav@gmail.com>
2022-05-23 14:10:13 +0800

    Init

---
+++ helper.py
@@ -0,0 +1,156 @@
+from math import sqrt
+import sqlite3
+from datetime import datetime
+import time
+from py_expression_eval import Parser
+import math
+import numpy
+
+meth = {"ceil":math.ceil, "comb":math.comb, "copysign":math.copysign, "fabs":math.fabs, "factorial":math.factorial, "floor":math.floor, "fmod":math.fmod, "frexp":math.frexp, "fsum":math.fsum, "gcd":math.gcd, "isclose":math.isclose, "isfinite":math.isfinite, "isinf":math.isinf, "isnan":math.isnan, "isqrt":math.isqrt, "ldexp":math.ldexp, "modf":math.modf, "perm":math.perm, "prod":math.prod, "remainder":math.remainder, "exp":math.exp, "expm1":math.expm1, "log":math.log, "log1p":math.log1p, "log2":math.log2, "log10":math.log10, "pow":math.pow, "sqrt":math.sqrt, "acos":math.acos, "asin":math.asin, "atan":math.atan, "atan2":math.atan2, "cos":math.cos, "dist":math.dist, "hypot":math.hypot, "sin":math.sin, "tan":math.tan, "degrees":math.degrees, "radians":math.radians, "acosh":math.acosh, "asinh":math.asinh, "atanh":math.atanh, "cosh":math.cosh, "sinh":math.sinh, "tanh":math.tanh, "erf":math.erf, "erfc":math.erfc, "gamma":math.gamma, "lgamma":math.lgamma, "pi":math.pi, "e":math.e, "tau":math.tau, "inf":math.inf, "nan":math.nan, "numpysin":numpy.sin, "numpycos":numpy.cos, "numpytan":numpy.tan, "numpyarcsin":numpy.arcsin, "numpyarccos":numpy.arccos, "numpyarctan":numpy.arctan, "numpyhypot":numpy.hypot, "numpyarctan2":numpy.arctan2, "numpydegrees":numpy.degrees, "numpyradians":numpy.radians, "numpyunwrap":numpy.unwrap, "numpydeg2rad":numpy.deg2rad, "numpyrad2deg":numpy.rad2deg, "numpysinh":numpy.sinh, "numpycosh":numpy.cosh, "numpytanh":numpy.tanh, "numpyarcsinh":numpy.arcsinh, "numpyarccosh":numpy.arccosh, "numpyarctanh":numpy.arctanh, "numpyaround":numpy.around, "numpyround_":numpy.round_, "numpyrint":numpy.rint, "numpyfix":numpy.fix, "numpyfloor":numpy.floor, "numpyceil":numpy.ceil, "numpytrunc":numpy.trunc, "numpyprod":numpy.prod, "numpysum":numpy.sum, "numpynanprod":numpy.nanprod, "numpynansum":numpy.nansum, "numpycumprod":numpy.cumprod, "numpycumsum":numpy.cumsum, "numpynancumprod":numpy.nancumprod, "numpynancumsum":numpy.nancumsum, "numpydiff":numpy.diff, "numpy.ediff1d":numpy.ediff1d, "numpygradient":numpy.gradient, "numpycross":numpy.cross, "numpytrapz":numpy.trapz, "numpyexp":numpy.exp, "numpyexpm1":numpy.expm1, "numpyexp2":numpy.exp2, "numpylog":numpy.log, "numpylog10":numpy.log10, "numpylog2":numpy.log2, "numpylog1p":numpy.log1p, "numpylogaddexp":numpy.logaddexp, "numpylogaddexp2":numpy.logaddexp2, "numpyi0":numpy.i0, "numpysinc":numpy.sinc,"numpysignbit":numpy.signbit, "numpycopysign":numpy.copysign, "numpyfrexp":numpy.frexp, "numpyldexp":numpy.ldexp, "numpynextafter":numpy.nextafter, "numpyspacing":numpy.spacing, "numpylcm":numpy.lcm, "numpygcd":numpy.gcd, "numpyadd":numpy.add, "numpyreciprocal":numpy.reciprocal, "numpypositive":numpy.positive, "numpynegative":numpy.negative, "numpymultiple":numpy.multiply, "numpydivide":numpy.divide, "numpypower":numpy.power, "numpy.subtract":numpy.subtract, "numpytrue_divide":numpy.true_divide, "numpyfloor_divide":numpy.floor_divide, "numpyfloat_power":numpy.float_power, "numpyfmod":numpy.fmod, "numpymod":numpy.mod, "numpymodf":numpy.modf, "numpyremainder":numpy.remainder, "numpydivmod":numpy.divmod, "numpyangle":numpy.angle, "numpyreal":numpy.real, "numpyimag":numpy.imag, "numpyconj":numpy.conj, "numpyconjugate":numpy.conjugate, "numpymaximum":numpy.maximum, "numpyfmax":numpy.fmax, "numpyamax":numpy.amax, "numpynanmax":numpy.nanmax, "numpyfmin":numpy.fmin, "numpyamin":numpy.amin, "numpynanmin":numpy.nanmin, "numpyconvolve":numpy.convolve, "numpyclip":numpy.clip, "numpysqrt":numpy.sqrt, "numpycbrt":numpy.cbrt, "numpysquare":numpy.square, "numpyabsolute":numpy.absolute, "numpyfabs":numpy.fabs, "numpysign":numpy.sign, "numpyheaviside":numpy.heaviside, "numpyreal_if_close":numpy.real_if_close, "numpyinterp":numpy.interp}
+#stoped at FPR
+
+parser = Parser()
+
+def top(guild_id:str) -> tuple :
+    db = sqlite3.connect(guild_id+".db")
+    cursor = db.cursor()
+    cursor.execute(f"SELECT counts, ID FROM mathematicians ORDER BY counts DESC LIMIT 10")
+    tops = cursor.fetchall()
+    db.close()
+    return tops
+
+def prime_top(guild_id:str) -> tuple :
+    db = sqlite3.connect(guild_id+".db")
+    cursor = db.cursor()
+    cursor.execute(f"SELECT primes, ID FROM mathematicians ORDER BY counts DESC LIMIT 10")
+    p_tops = cursor.fetchall()
+    db.close()
+    return p_tops
+
+def top_fail(guild_id:str) -> tuple :
+    db = sqlite3.connect(guild_id+".db")
+    cursor = db.cursor()
+    cursor.execute(f"SELECT fails, ID FROM mathematicians ORDER BY counts DESC LIMIT 10")
+    f_tops = cursor.fetchall()
+    db.close()
+    return f_tops
+
+def is_prime(n:float) -> bool: 
+    n = int(n)
+    prime_flag = 0
+    if(n > 1):
+        for i in range(2, int(sqrt(n)) + 1):
+            if (n % i == 0):
+                prime_flag = 1
+                break
+        if (prime_flag == 0):
+            return True
+        else:
+            return False
+    else:
+        return False
+        
+def next_prime(guild_id:str) -> int :
+    db = sqlite3.connect(guild_id+".db")
+    cursor = db.cursor()
+    cursor.execute(f"SELECT count from server WHERE serverID={int(guild_id)}")
+    no = int(cursor.fetchone()[0])
+    db.close()
+    while not is_prime(no):
+        no += 1
+    return no
+        
+def init_db(guild_id:str, text_channel_id:int):
+    db = sqlite3.connect(guild_id+".db")
+    db.execute("CREATE TABLE server (serverID PRIMARY KEY, count_channel INT, count INT NOT NULL, fail_role INT, last_counter TEXT, high_score INT NOT NULL, base TEXT NOT NULL, fails INT NOT NULL, counts INT NOT NULL) ")
+    db.execute("CREATE TABLE mathematicians (ID PRIMARY KEY, fails INT, counts INT, high_score INT, last_fail INT, last_fail_date TEXT, last_count INT, delta_fail INT, primes INT)")
+    db.close()
+    db = sqlite3.connect(guild_id+".db")
+    cursor = db.cursor()
+    cursor.execute("INSERT INTO server (serverID, count_channel, count, high_score, base, fails, counts) VALUES (?, ?, ?, ?, ?, ?, ?)", (int(guild_id), text_channel_id, 1, 0, "10", 0, 0))
+    db.commit()
+    db.close()
+
+def user_stats(guild_id:str, mathematician:int) -> tuple :
+    db = sqlite3.connect(guild_id+".db")
+    cursor = db.cursor()
+    cursor.execute(f"SELECT last_count, counts, fails, high_score, last_fail, last_fail_date, primes FROM mathematicians WHERE ID={mathematician}")
+    stats = cursor.fetchone()
+    db.close()
+    return stats
+
+def count_channel(guild_id:str) -> int :
+    db = sqlite3.connect(guild_id+".db")
+    cursor = db.cursor()
+    cursor.execute(f"SELECT count_channel FROM server WHERE serverID={int(guild_id)}")
+    channel = cursor.fetchone()
+    db.close()
+    return int(channel[0])
+
+def change_count_channel(guild_id:str, text_channel_id:int):
+    db = sqlite3.connect(guild_id+".db")
+    cursor = db.cursor()
+    cursor.execute(f"UPDATE server SET count_channel={int(text_channel_id)}")
+    db.commit()
+    db.close()
+
+def fail_role(guild_id:str) -> int :
+    db = sqlite3.connect(guild_id+".db")
+    cursor = db.cursor()
+    cursor.execute(f"SELECT fail_role FROM server WHERE serverID={int(guild_id)}")
+    role_id = cursor.fetchone()[0]
+    db.close()
+    return role_id
+    
+
+def change_fail_role(guild_id:str, fail_role_id:int):
+    db = sqlite3.connect(guild_id+".db")
+    cursor = db.cursor()
+    cursor.execute(f"UPDATE server SET fail_role={int(fail_role_id)}")
+    db.commit()
+    db.close()
+    
+def count(expression:str, guild_id:str, mathematician:int) -> tuple :
+    db = sqlite3.connect(guild_id+".db")
+    cursor = db.cursor()
+    lzs = cursor.execute(f"SELECT ID FROM mathematicians").fetchall()
+    eep = []
+    for i in lzs:
+        eep.append(i[0])
+    if mathematician not in eep:
+        cursor.execute(f"INSERT INTO mathematicians (ID, fails, counts, high_score, last_fail, last_fail_date, last_count, delta_fail, primes) VALUES ({mathematician}, 0, 0, 0, 0, 'None', 0, 0, 0)")
+        db.commit()
+    try:
+        number = parser.parse(expression).evaluate({})
+    except:
+        number = 0
+    try:
+        number2 = eval(expression,{},meth)
+    except:
+        number2 = 0
+    
+    cursor.execute(f"SELECT count from server WHERE serverID={int(guild_id)}")
+    no = int(cursor.fetchone()[0])
+    cursor.execute(f"SELECT last_counter from server WHERE serverID={int(guild_id)}")
+    last_math = cursor.fetchone()[0]
+    last_math = int(last_math if last_math != None else 0)
+    if (number == 0 and number2 == 0) :
+        return (69, None)
+    elif (no == number or no == number2) and not (mathematician == last_math):
+        new_c = cursor.execute(f"SELECT counts from mathematicians WHERE ID={mathematician}").fetchone()[0] + 1
+        new_d = cursor.execute(f"SELECT delta_fail from mathematicians WHERE ID={mathematician}").fetchone()[0] + 1
+        cursor.execute(f"UPDATE mathematicians SET counts={new_c}, high_score={no if no>number else number}, last_count={no}, delta_fail={new_d} WHERE ID={mathematician}")   
+        cursor.execute(f"UPDATE server SET count={no+1}, last_counter={mathematician} WHERE serverID={int(guild_id)}")
+        if (is_prime(number or is_prime(number2))):
+            new_p = cursor.execute(f"SELECT primes from mathematicians WHERE ID={mathematician}").fetchone()[0] + 1
+            cursor.execute(f"UPDATE mathematicians SET primes={new_p} WHERE ID={mathematician}")
+        db.commit()
+        db.close()
+        return (True, (is_prime(number or is_prime(number2))))
+    else:
+        new_f = cursor.execute(f"SELECT fails from mathematicians WHERE ID={mathematician}").fetchone()[0] + 1
+        cursor.execute(f"UPDATE mathematicians SET fails={new_f}, last_fail={no}, last_fail_date={time.time()} WHERE ID={mathematician}")
+        cursor.execute(f"UPDATE server SET count=1, last_counter={mathematician} WHERE serverID={int(guild_id)}")
+        db.commit()
+        db.close()
+        return (False, None)
---
+++ main.py
@@ -0,0 +1,150 @@
+import os, time
+import discord
+from discord.ext import commands
+from discord.utils import get
+import helper
+import glob
+
+with open("token.txt") as f:
+    token = f.readline()
+
+
+client = commands.Bot(command_prefix="%")
+client.remove_command("help")
+
+@client.event
+async def on_ready():
+    print("We are online")
+    
+@client.event
+async def on_message(message):
+    await client.process_commands(message)
+    if message.author.bot == True:
+        return
+    else:
+        if (os.getcwd() + "/" + str(message.guild.id) + ".db") not in glob.glob(os.getcwd()+"/*.db"):
+            helper.init_db(str(message.guild.id), message.channel.id)
+            await (message.channel).send("Initialised DB.")
+        else:
+            if message.channel.id != helper.count_channel(str(message.guild.id)):
+                return
+            res = helper.count(message.content, str(message.guild.id), int(message.author.id)) 
+            if res[0] == 69:
+                return
+            elif res[0] == False:
+                await message.add_reaction("\U0000274C")
+                await (message.channel).send(f"<@{int(message.author.id)}> messed up! Count restarts at 1") 
+                await message.author.add_roles(get(message.guild.roles, id=int(helper.fail_role(str(message.guild.id)))),"You can't even count",True)
+            elif res[1] == False:
+                await message.add_reaction("\U00002705")
+            else:
+                await message.add_reaction("\U00002611")
+    
+@client.command()
+async def help(ctx):
+    reply = discord.Embed(title="The Better Counting Bot")
+    reply.add_field(name="__**Commands**__", value="Admins only! (other than help of course)", inline=False)
+    reply.add_field(name="%help", value="Sends help message", inline=False)
+    reply.add_field(name="%set_channel", value="Set counting channel, defaults to all", inline=False)
+    reply.add_field(name="%set_fail_role", value="Set role given to person who counts wrongly", inline=False)
+    reply.add_field(name="%user_stats [@user]", value="Gets user's stats, if no user is mentioned, it gets the sender's stats", inline=False)
+    reply.add_field(name="%set_base", value="Set the base of the number system to be used. Default Base 10. (Work in progress)", inline=False)
+    reply.add_field(name="%top", value="Top counters", inline=False)
+    reply.add_field(name="%prime_top", value="Top prime number counters", inline=False)
+    reply.add_field(name="%fail_top", value="Top failures", inline=False)
+    reply.add_field(name="%next_prime", value="Returns the next prime number", inline=False)
+    reply.add_field(name="__**Counting**__", value="Green tick - Correct \n Blue tick - Correct and prime number \n You cannot count consecutively, even after you mess up. ", inline=False)
+    reply.add_field(name="__**Special Operators**__", value="Python math library functions and numpy library functions. For numpy, add 'numpy' before the function, e.g. numpycumsum", inline=False)
+    await ctx.send(embed=reply)
+
+@client.command()
+async def next_prime(ctx):
+    await ctx.send(f"The next prime number is {helper.next_prime(str(ctx.message.guild.id))}")
+    
+@client.command()
+async def top(ctx):
+    top_math = helper.top(str(ctx.message.guild.id))
+    reply = discord.Embed(title="Top Counters!")
+    for i in top_math:
+        reply.add_field(name=f"<@!{i[1]}>", value=f"{i[0]} Counts", inline=False)
+    await ctx.send(embed=reply)
+    
+@client.command()
+async def prime_top(ctx):
+    prime_math = helper.prime_top(str(ctx.message.guild.id))
+    reply = discord.Embed(title="Top Prime Counters!")
+    for i in prime_math:
+        reply.add_field(name=f"<@!{i[1]}>", value=f"{i[0]} Prime Numbers", inline=False)
+    await ctx.send(embed=reply)
+    
+@client.command()
+async def fail_top(ctx):
+    top_fail = helper.top_fail(str(ctx.message.guild.id))
+    reply = discord.Embed(title="Top Failures!")
+    for i in top_fail:
+        reply.add_field(name=f"<@!{i[1]}>", value=f"{i[0]} Fails", inline=False)
+    await ctx.send(embed=reply)
+    
+
+    
+@client.command()
+async def set_channel(ctx):
+    prev = helper.count_channel(str(ctx.message.guild.id))
+    if ctx.message.channel.id != prev:
+        await ctx.send(f"Please run this command in the current counting channel, <#{prev}>.")
+        return
+    elif ctx.message.author.permissions_in(ctx.message.channel).administrator != True:
+        await ctx.send("Your're not an Admin.")
+        return
+    try: 
+        (ctx.message).raw_channel_mentions[0]
+    except IndexError:
+        await ctx.send("Mention a channel asshole.")
+    else:
+        if ctx.message.channel_mentions[0] not in ctx.guild.text_channels:
+            await ctx.send("That's not a valid channel idiot.")
+            return
+        else:
+            helper.change_count_channel(str(ctx.message.guild.id), (ctx.message).raw_channel_mentions[0])
+            await ctx.send(f"Changed counting channel from <#{prev}> to <#{(ctx.message).raw_channel_mentions[0]}>")
+        
+@client.command()
+async def set_fail_role(ctx):
+    prev = helper.count_channel(str(ctx.message.guild.id))
+    if ctx.message.channel.id != prev:
+        await ctx.send(f"Please run this command in the current counting channel, <#{prev}>.")
+        return
+    elif ctx.message.author.permissions_in(ctx.message.channel).administrator != True:
+        await ctx.send("Your're not an Admin.")
+        return
+    
+    try:
+        ctx.message.raw_role_mentions[0]
+    except IndexError:
+        await ctx.send("Mention a role asshole.")
+    else:
+        helper.change_fail_role(str(ctx.message.guild.id), ctx.message.raw_role_mentions[0])
+        await ctx.send(f"Changed fail role to <@{ctx.message.raw_role_mentions[0]}>")
+
+@client.command()
+async def set_base(ctx):
+    await ctx.send("Not implemented yet, Base 10 default applies.")
+    
+@client.command()
+async def user_stats(ctx):
+    try:
+        user = ((ctx.message.mentions)[0]).id
+    except IndexError:
+        user = ctx.message.author.id
+    stats = helper.user_stats(str(ctx.message.guild.id), user)
+    reply = discord.Embed(title=f"<@!{user}> stats")
+    reply.add_field(name="Last Number Counted", value=f"{stats[0]}", inline=False)
+    reply.add_field(name="Number of Counts", value=f"{stats[1]}", inline=False)
+    reply.add_field(name="Number of Fails", value=f"{stats[2]}", inline=False)
+    reply.add_field(name="Highest Count", value=f"{stats[3]}", inline=False)
+    reply.add_field(name="Last Failed Number", value=f"{stats[4]}", inline=False)
+    reply.add_field(name="Last Failure Date", value=f"{stats[5]}", inline=False)
+    reply.add_field(name="Number of Primes Counted", value=f"{stats[6]}", inline=False)
+    await ctx.send(embed=reply)
+    
+client.run(TOKEN)
